8c7dffa700a3a2cf807d2059176db9763fa901e9
MJ LEE <mj1201276@yahoo.com.au>
2022-03-12 18:55:30 +1000

    Updated escrow code

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,21 +1,12 @@
+use spl_token::error::TokenError::InvalidInstruction;
+
 use std::convert::TryInto;
 use solana_program::program_error::ProgramError;
 
-use crate::error::EscrowError:InvalidInstruction;
+use solana_program::error::EscrowError::InvalidInstruction;
 
 pub enum EscrowInstruction {
 
-    /// Starts the trade by creating and populating an escrow account and transferring ownership of the given temp token account to the PDA
-    ///
-    ///
-    /// Accounts expected:
-    ///
-    /// 0. `[signer]` The account of the person initializing the escrow
-    /// 1. `[writable]` Temporary token account that should be created prior to this instruction and owned by the initializer
-    /// 2. `[]` The initializer's token account for the token they will receive should the trade go through
-    /// 3. `[writable]` The escrow account, it will hold all necessary info about the trade.
-    /// 4. `[]` The rent sysvar
-    /// 5. `[]` The token program
     InitEscrow {
         /// The amount party A expects to receive of token Y
         amount: u64
@@ -34,7 +25,7 @@ impl EscrowInstruction {
             _ => return Err(InvalidInstruction.into()),
         })
     }
-
+    
     fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
         let amount = input
             .get(..8)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod entrypoint;
-pub mod instruction.rs;
+pub mod instruction;
+pub mod processor;
 
